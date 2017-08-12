abcd383a6b1a2d580e1d3eaf4c4268b2b3eaa5db
Josh Forbes <jforbes@kimmel.com>
2017-08-12 08:51:47 -0400

    Removing uneeded as:json option

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -9,7 +9,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
             email: user.email,
             password: user.password
         }
-    }, as: :json
+    }
 
     assert_response 200
     assert_equal user.token.body, json_response['token']
@@ -23,7 +23,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
             email: user.email,
             password: 'Invalid Password'
         }
-    }, as: :json
+    }
 
     assert_response 401
   end
@@ -32,7 +32,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     user = create(:user)
     original_token = user.token.body
 
-    delete sessions_url, as: :json, headers: auth_headers(user)
+    delete sessions_url, headers: auth_headers(user)
 
     assert_response 204
     assert_not_equal original_token, user.reload.token.body
