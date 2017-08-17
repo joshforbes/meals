2f247dad32b4c53502c9f50a25e49ab7204c3828
Josh Forbes <jforbes@kimmel.com>
2017-08-16 22:59:17 -0400

    Adding the user to the recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :name
+
+  has_one :user, embed: :ids
 end
--- test/serializers/recipe_serializer_test.rb
+++ test/serializers/recipe_serializer_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class RecipeSerializerTest < ActiveSupport::TestCase
   def serialized_recipe(recipe)
-    {id: recipe.id, user_id: recipe.user_id, name: recipe.name}.to_json
+    {
+      id: recipe.id,
+      user_id: recipe.user_id,
+      name: recipe.name,
+      user: UserSerializer.new(recipe.user)
+    }.to_json
   end
 
   test 'it serializes a recipe' do
