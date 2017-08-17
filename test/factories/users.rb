e2507e70127257cde9b93802d9a99747da64a088
Josh Forbes <jforbes@kimmel.com>
2017-08-16 22:27:05 -0400

    Put the password param in a block

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
-    password Faker::Internet.password
+    password { Faker::Internet.password }
     token_last_used_at DateTime.now
   end
 end
